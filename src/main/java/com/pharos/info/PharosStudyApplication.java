3deadbd8b1142bdd1fb266d7301ef9676e3ac829
KsSim93 <93sks@naver.com>
2024-01-18 13:16:27 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/pharos/info/PharosStudyApplication.java
@@ -0,0 +1,13 @@
+package com.pharos.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharosStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharosStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharos/info/PharosStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharos.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharosStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
